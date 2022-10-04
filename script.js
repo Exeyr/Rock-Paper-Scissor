e2b7bdfee5c516e83f10c10865cde3989a5550f6
Exeyr <peep.podder@gmail.com>
2022-10-04 16:35:54 +0300

    Moved some comments around so the algorithm would make sense. No actual coding changes

--- script.js
+++ script.js
@@ -27,6 +27,13 @@ function PlayRound(PlayerChoice) {
     
     if (PlayerChoice === ComputerChoice) {
         document.getElementById('ResultsText').innerHTML = "It's a Tie!";
+        // 3.1 PlayerChoice = Rock, then
+        // 3.1.1 ComputerChoice = Rock -> "It's a Tie!"
+        // 3.2 Player Choice = Paper, then
+        // 3.2.2 ComputerChoice = Paper -> "It's a Tie!"
+        // 3.3 PlayerChoice = Scissors, then
+        // 3.3.3 ComputerChoice = Scissors -> "It's a Tie!"
+        // 4. Display the result on the webpage
     } 
     else if (
         (PlayerChoice === "Rock" && ComputerChoice === "Scissors") ||
@@ -34,6 +41,13 @@ function PlayRound(PlayerChoice) {
         (PlayerChoice === "Scissors" && ComputerChoice === "Paper")
     ) {
         document.getElementById('ResultsText').innerHTML = "You Win!";
+        // 3.1 PlayerChoice = Rock, then
+        // 3.1.3 ComputerChoice = Scissors -> "You Win!"
+        // 3.2 Player Choice = Paper, then
+        // 3.2.1 ComputerChoice = Rock -> "You Win!"
+        // 3.3 PlayerChoice = Scissors, then
+        // 3.3.2 ComputerChoice = Paper -> "You Win!"
+        // 4. Display the result on the webpage
     } 
     else if (
         (PlayerChoice === "Rock" && ComputerChoice === "Paper") ||
@@ -41,24 +55,16 @@ function PlayRound(PlayerChoice) {
         (PlayerChoice === "Scissors" && ComputerChoice === "Rock")
         ) {
         document.getElementById('ResultsText').innerHTML = "You Lose!";
+        // 3.1 PlayerChoice = Rock, then
+        // 3.1.2 ComputerChoice = Paper -> "You Lose!"
+        // 3.2 Player Choice = Paper, then
+        // 3.2.3 ComputerChoice = Scissors -> "You Lose!"
+        // 3.3 PlayerChoice = Scissors, then
+        // 3.3.1 ComputerChoice = Rock -> "You Lose!"
+        // 4. Display the result on the webpage
     }
     
 }
     
-
-
-// 3.1 PlayerChoice = Rock, then
-// 3.1.1 ComputerChoice = Rock -> "It's a Tie!"
-// 3.1.2 ComputerChoice = Paper -> "You Lose!"
-// 3.1.3 ComputerChoice = Scissors -> "You Win!"
-// 3.2 Player Choice = Paper, then
-// 3.2.1 ComputerChoice = Rock -> "You Win!"
-// 3.2.2 ComputerChoice = Paper -> "It's a Tie!"
-// 3.2.3 ComputerChoice = Scissors -> "You Lose!"
-// 3.3 PlayerChoice = Scissors, then
-// 3.3.1 ComputerChoice = Rock -> "You Lose!"
-// 3.3.2 ComputerChoice = Paper -> "You Win!"
-// 3.3.3 ComputerChoice = Scissors -> "It's a Tie!"
-// 4. Display the result on the webpage
 // 5. Have an option to play a GAME of 5 rounds that keeps score
 // 6. Have a reset button to essentially referesh the page
