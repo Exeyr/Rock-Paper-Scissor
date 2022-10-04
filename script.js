debd951b04a695e7cbc0094ff796899a3e1a3cce
Exeyr <peep.podder@gmail.com>
2022-10-04 13:02:27 +0300

    Trying still to get the buttons to work properly. It seems like that both the array method for computer choice and the queryselector method for player choice work and retain the information, but I can't figure out the reason why the results of the script won't display. It can either be a problem of improper display command or some problem with the variables themselves in the Javascript file

--- script.js
+++ script.js
@@ -4,9 +4,33 @@ const PossibleChoices = ["Rock", "Paper", "Scissors"];
 var ComputerChoice = PossibleChoices[Math.floor(Math.random() * 3)]
 // Check 1. -> This array method works, it chooses a random variable each time
 // 2. Get the players input of Rock, Paper or Scissors
-// 2.5 Do this with a series of buttons labeled "Rock", "Paper" and "Scissors"
-// Check 2. and 2.5 -> Buttons have been created, in 3. will attatch functions to them
+// 2.1 Do this with a series of buttons labeled "Rock", "Paper" and "Scissors"
+// 2.2 Store the players choice in a new variable called player choice
+let 
+    PlayerChoice = 
+
+document.querySelector('#Rock').onclick = function() {
+    PlayerChoice = "Rock";
+}
+document.querySelector('#Paper').onclick = function() {
+    PlayerChoice = "Paper";
+}
+document.querySelector("#Scissors").onclick = function () {
+    PlayerChoice = "Scissors";
+}
+
 // 3. Have a function compare the players choice to the computers based on a set of rules
+function PlayRound() {
+
+    if (PlayerChoice === ComputerChoice) {
+        document.getElementById('ResultsText').innerHTML = "It's a Tie!";
+    } else {
+        document.getElementById('ResultsText').innerHTML = "We'll get to that!";
+    }
+}
+    
+
+
 // 3.1 PlayerChoice = Rock, then
 // 3.1.1 ComputerChoice = Rock -> "It's a Tie!"
 // 3.1.2 ComputerChoice = Paper -> "You Lose!"
